bf42d4a659c35b3b41f643308311e92b964cd352
FuekongChang <48033334+FuekongChang@users.noreply.github.com>
2019-05-08 15:02:16 -0700

    added an home.php

---
+++ home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Homepage</title>
+    <link href="Css/main.css" rel="stylesheet" type="text/css">
+    <script type="text/javascript"></script>
+</head>
+<body>
+   
+    <?php
+    
+    ?>
+</body>
+</html>
