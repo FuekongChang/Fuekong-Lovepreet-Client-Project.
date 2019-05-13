18df654c1043222e2edafb527dfad1218c60f58b
LovepreetUppal <lovepreetuppal007@gmail.com>
2019-05-13 15:18:30 -0700

    finished header

--- header.php
+++ header.php
@@ -1,17 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Uppal Transport</title>
     <link rel="stylesheet" href="Css/main.css">
 </head>
+
 <body>
     <div class="navbar">
-        <li><a href="home.php">Home</a></li>
-        <div class="vl"></div>
-        <li><a href="services.php">Services</a></li>
-        <div class="vl"></div>
-        <li><a href="contact.php">Contact</a></li>
+        <div class="logo">
+            <img class="lo" src="image-folder/uppal.PNG" alt="company-logo">
+        </div>
+        <div class="page">
+            <li><a href="home.php">Home</a></li>
+            <!--            <div class="vl"></div>-->
+            <li><a href="services.php">Services</a></li>
+            <!--            <div class="vl"></div>-->
+            <li class="co"><a href="contact.php">Contact</a></li>
+        </div>
     </div>
+
 </body>
+
 </html>
