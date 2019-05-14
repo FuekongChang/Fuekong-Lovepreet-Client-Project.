761830068b760d5bea6813d530ff0eb4886de940
LovepreetUppal <lovepreetuppal007@gmail.com>
2019-05-14 14:34:36 -0700

    copyright on footer

---
+++ footer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Footer</title>
+    <link rel="stylesheet" href="Css/main.css">
+</head>
+
+<body>
+    <footer class="footer">
+        <div class="foot-info">
+            2019 &copy; All Rights Reserved. Uppal Transport Llc
+        </div>
+    </footer>
+</body>
+
+</html>
