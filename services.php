000a7bd66261ad6d12d066cead9f45e2b7247b5c
LovepreetUppal <lovepreetuppal007@gmail.com>
2019-05-14 15:28:41 -0700

    end of tuesday

---
+++ services.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Services</title>
+    <link rel="stylesheet" href="Css/main.css">
+</head>
+
+<body>
+   <div class="parallax-ser"></div>
+    <div class="ser1">
+        <div class="single">
+            <p class="ser-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis laborum, praesentium harum necessitatibus laudantium nobis iusto reiciendis obcaecati quis maiores quas, beatae. Earum animi aliquid, fugit unde. Odio, voluptate, id.</p>
+        </div>
+
+        <div class="team">
+            <p class="ser-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis laborum, praesentium harum necessitatibus laudantium nobis iusto reiciendis obcaecati quis maiores quas, beatae. Earum animi aliquid, fugit unde. Odio, voluptate, id.</p>
+        </div>
+    </div>
+</body>
+
+</html>
