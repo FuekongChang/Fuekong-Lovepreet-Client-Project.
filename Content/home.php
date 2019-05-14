af8d4e67360e0d001b6f52689bdb306af9c79896
CHANG FUEKONG <567681@cart.ads>
2019-05-14 15:28:37 -0700

    I hate today.

---
+++ Content/home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html class="home" lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Uppal Transport</title>
+  <link rel="stylesheet" href="css.css">
+</head>
+<body>
+   <div class="flex-container">
+   <div class="hometitle">
+    <h1>Uppal Transport</h1>
+    </div>
+<div class="homepage1"><p><strong>Welcome!</strong> We are Uppal Transport. We specalize in the transportation of goods.</p>
+ </div>
+  <div class="homepage2">
+      <p>To use our serves you can send us an email through either your personal computer, laptop, phone etc..We also use a reefer trailer to keep items cold or frozen for long period of time and for temperature-sesitive items. They can also be used for heated items too.We also provide dry van transportation to keep your items nice and dry through the reain or any other conditions and to keep it safe obviously.</p></div>
+   <p>We also are located in the Fresno city and contact you if we any question about the cargo you need carried.For more information there is a link here.</p>
+    =
+       <div class="header2">
+   <h2>Our Purpose</h2>
+       </div>
+       <div class="paragraph">
+    <p>We strive to get the job well done in a well fashion, so you can be happy and we know we did good.We also wanted to provide a great deal of servise to many people as we can so it wont be a hassle for those that need things to get moved faster and safly.</p>
+       </div>
+    
+    <h2>Extra Inofrmation</h2>
+    
+    <p>If you want more information about our servuise take a look at our other page.</p>
+    </div>
+    <?php
+  
+   
+    ?>
+   
+    
+</body>
+</html>
--- connect.php
+++ connect.php
@@ -3,29 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <title>Uppal Transport</title>
-   
+     <link rel="stylesheet" href="css.css">
+
 </head>
 <body>
-
-    <?php
-    require('moduals/header.php');
+ <?php
+    require("Moduals/header.php");
     
     if(isset($_GET['page'])){
         $page = $_GET['page'];
-        if(file_exists("?page.php")){
+        if(file_exists("Content/$page.php")){
         
-            include("$page.php");
+            include("Content/$page.php");
             
         }
-        else{
+        else {
             echo "<h1>404: You MAY NOT PASS!</h1>";
         }
     }
-    else{
-        include("home.php");
+    else {
+        include("Content/home.php");
     }
     
-    require('moduals/footer.php');
+    require("Moduals/footer.php");
     ?>
+    
 </body>
 </html>
