de65107f8380355372400181219b4a69203bff5f
LovepreetUppal <lovepreetuppal007@gmail.com>
2019-05-16 15:11:04 -0700

    map and form for the contact page

---
+++ contact.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Contact</title>
+    <link rel="stylesheet" href="Css/main.css">
+</head>
+
+<body>
+    <div class="page2">
+        <div class="con-form">
+            <form action="" class="form">
+                <label for="name">Name: </label><br>
+                <input type="text" name="name" value=""><br>
+
+                <label for="email">Email Address:</label><br>
+                <input type="email" name="email" placeholder=""><br>
+
+                <label for="interest">Interested in:</label><br>
+                <select name="interest" id="reason">
+                    <option value=""></option>
+                    <option value="D">Driving</option>
+                    <option value="O">Owner Operator</option>
+                    <option value="OT">Other</option>
+                </select><br>
+
+                <label for="comment">Comment:</label><br>
+                <textarea name="comment" id="comment" cols="30" rows="10"></textarea>
+            </form>
+        </div>
+        <div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3797.4669796630074!2d-119.91369684846231!3d36.83585662546809!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80946f1d003c7967%3A0x7797d5ca6b40eb9d!2s6797+W+Parr+Ave%2C+Fresno%2C+CA+93722!5e0!3m2!1sen!2sus!4v1558042536037!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+    </div>
+</body>
+
+</html>
--- services.php
+++ services.php
@@ -8,14 +8,30 @@
 </head>
 
 <body>
-   <div class="parallax-ser"></div>
+    <div class="parallax-ser"></div>
     <div class="ser1">
         <div class="single">
+            <h3 class="ser-h3">Solo</h3>
             <p class="ser-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis laborum, praesentium harum necessitatibus laudantium nobis iusto reiciendis obcaecati quis maiores quas, beatae. Earum animi aliquid, fugit unde. Odio, voluptate, id.</p>
         </div>
 
         <div class="team">
+            <h3 class="ser-h3">Team</h3>
             <p class="ser-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis laborum, praesentium harum necessitatibus laudantium nobis iusto reiciendis obcaecati quis maiores quas, beatae. Earum animi aliquid, fugit unde. Odio, voluptate, id.</p>
+        </div>
+    </div>
+    <div class="truck">
+        <h2>Trucks</h2>
+    </div>
+    <div class="ser2">
+        <div class="cas">
+            <h3>Cascadia</h3>
+
+            <p class="ser-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis mollitia necessitatibus veniam deserunt rerum aperiam asperiores labore eaque pariatur hic iste doloribus animi nesciunt architecto, dignissimos ullam quibusdam reiciendis molestias?</p>
+        </div>
+        <div class="ken">
+            <h3>Kenworth</h3>
+            <p class="ser-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis mollitia necessitatibus veniam deserunt rerum aperiam asperiores labore eaque pariatur hic iste doloribus animi nesciunt architecto, dignissimos ullam quibusdam reiciendis molestias?</p>
         </div>
     </div>
 </body>
