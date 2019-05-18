d2c01b5237c75cfda0b553dbf85d297ac57dcad1
Lovepreet Uppal <lovepreetuppal007@gmail.com>
2019-05-17 23:18:50 -0700

    finished services page

--- contact.php
+++ contact.php
@@ -2,50 +2,53 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <title>Contact</title>
-    <link rel="stylesheet" href="Css/main.css">
+  <meta charset="UTF-8">
+  <title>Contact</title>
+  <link rel="stylesheet" href="Css/main.css">
 </head>
 
 <body class="body-c">
-    <div class="parallax-c"></div>
-    <div class="con-info">Connect With One of Our Transportation Expert Today by Our phone or email</div>
+  <div class="parallax-c"></div>
+  <div class="con-info">Connect With One of Our Transportation Expert Today by Phone or Email. Send us your name and contact information from the form below and we'll ger back to you as soon as possible</div>
 
 
-    <div class="address">
-        <h3 class="address-h">Contact Information:</h3>
-        <p class="address-p">
-            6797 W. Parr Ave<br>
-            Fresno, CA 93722<br><br>
-            601 - 695 - 8806<br><br>
-            uppal.transport@yahoo.com
-            
-        </p>
+  <div class="address">
+    <h3 class="address-h">Contact Information:</h3>
+    <p class="address-p">
+      6797 W. Parr Ave<br>
+      Fresno, CA 93722<br><br>
+      601 - 695 - 8806<br><br>
+      uppal.transport@yahoo.com
+
+    </p>
+  </div>
+  <div class="page2">
+    <div class="con-form">
+      <form action="" class="form">
+        <label for="name">Name: </label><br>
+        <input type="text" name="name" value=""><br>
+
+        <label for="email">Email Address:</label><br>
+        <input type="email" name="email" placeholder=""><br>
+
+        <label for="interest">Interested in:</label><br>
+        <select name="interest" id="reason">
+          <option value=""></option>
+          <option value="D">Driving</option>
+          <option value="O">Owner Operator</option>
+          <option value="OT">Other</option>
+        </select><br>
+
+        <label for="comment">Comment:</label><br>
+        <textarea name="comment" id="comment" cols="25" rows="7"></textarea>
+      </form>
     </div>
-    <div class="page2">
-        <div class="con-form">
-            <form action="" class="form">
-                <label for="name">Name: </label><br>
-                <input type="text" name="name" value=""><br>
-
-                <label for="email">Email Address:</label><br>
-                <input type="email" name="email" placeholder=""><br>
-
-                <label for="interest">Interested in:</label><br>
-                <select name="interest" id="reason">
-                    <option value=""></option>
-                    <option value="D">Driving</option>
-                    <option value="O">Owner Operator</option>
-                    <option value="OT">Other</option>
-                </select><br>
-
-                <label for="comment">Comment:</label><br>
-                <textarea name="comment" id="comment" cols="25" rows="7"></textarea>
-            </form>
-        </div>
-        <div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3797.4669796630074!2d-119.91369684846231!3d36.83585662546809!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80946f1d003c7967%3A0x7797d5ca6b40eb9d!2s6797+W+Parr+Ave%2C+Fresno%2C+CA+93722!5e0!3m2!1sen!2sus!4v1558042536037!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-        </div>
+    <div class="map"><iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3797.4669796630074!2d-119.91369684846231!3d36.83585662546809!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80946f1d003c7967%3A0x7797d5ca6b40eb9d!2s6797+W+Parr+Ave%2C+Fresno%2C+CA+93722!5e0!3m2!1sen!2sus!4v1558042536037!5m2!1sen!2sus"
+        width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
     </div>
+  </div>
+
 </body>
 
 </html>
